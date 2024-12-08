8b1e91b8b25239818feb3b39302151671016cfc0
Azya <AzyaDr@yandex.ru>
2024-12-09 00:45:46 +0300

    Add TSK_309

---
+++ Classes and methods/TSK_309/Main.java
@@ -0,0 +1,19 @@
+package TSK_309;
+
+public class Main {
+    // Ответ в answer.md
+    @SuppressWarnings("removal")
+    public static void main(String[] args) {
+        A a = new A();
+        Number[ ] num = {new Integer(1), 11, 1.11f, 11.11 };
+        // цикл for (1)
+        for (Number n : num) {
+            a.printNum(n);
+        }
+        // (2)
+        a.printNum(new Integer(1));
+        a.printNum(11);
+        a.printNum(1.11f);
+        a.printNum(11.11);
+    }
+}
