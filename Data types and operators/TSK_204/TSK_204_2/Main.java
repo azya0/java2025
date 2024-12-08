66e7e3b36bb901b52911bb42848a8d6f4be368b8
Azya <AzyaDr@yandex.ru>
2024-12-08 17:48:36 +0300

    Update TSK_204_2

--- Data types and operators/TSK_204/TSK_204_2/Main.java
+++ Data types and operators/TSK_204/TSK_204_2/Main.java
@@ -4,11 +4,19 @@ public class Main {
     public static void main(String[] arg) {
         // До 9ой версии:
 
+        @SuppressWarnings("removal")
         Boolean bool1 = new Boolean(true);
+
+        @SuppressWarnings("removal")
         Boolean bool2 = new Boolean(false);
-      
+        
+        @SuppressWarnings("removal")
         Boolean bool3 = new Boolean("true"); // true
+
+        @SuppressWarnings("removal")
         Boolean bool4 = new Boolean("false"); // false
+
+        @SuppressWarnings("removal")
         Boolean bool5 = new Boolean("random"); // false
         
         // Среда разработки подсказывает, что:
@@ -22,5 +30,18 @@ public class Main {
         Boolean bool8 = Boolean.valueOf("TRUE");   // true
         Boolean bool9 = Boolean.valueOf("false");   // false
         Boolean bool10 = Boolean.valueOf("random");  // false
+
+        System.out.println(
+            bool1 + " " +
+            bool2 + " " +
+            bool3 + " " +
+            bool4 + " " +
+            bool5 + " " +
+            bool6 + " " +
+            bool7 + " " +
+            bool8 + " " +
+            bool9 + " " +
+            bool10
+        );
     }
 }
