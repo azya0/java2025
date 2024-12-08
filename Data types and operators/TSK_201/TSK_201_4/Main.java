f171a10d35f85366f8e9fb90df95e832f972275d
Azya <AzyaDr@yandex.ru>
2024-12-08 16:18:04 +0300

    Add TSK_201_4

---
+++ Data types and operators/TSK_201/TSK_201_4/Main.java
@@ -0,0 +1,21 @@
+package TSK_201.TSK_201_4;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello";
+        int num = 42;
+        double pi = 3.14;
+
+        // Конкатенация строки с int
+        String result1 = str + " " + num; // "Hello 42"
+        System.out.println(result1);
+
+        // Конкатенация строки с double
+        String result2 = str + " " + pi; // "Hello 3.14"
+        System.out.println(result2);
+
+        // Конкатенация нескольких значений
+        String result3 = str + " " + num + " " + pi; // "Hello 42 3.14"
+        System.out.println(result3);
+    }
+}
