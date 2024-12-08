ff9ff8cff610aac443f1522f68116c8cbf804138
Azya <AzyaDr@yandex.ru>
2024-12-08 16:34:24 +0300

    Add TSK_201_7

---
+++ Data types and operators/TSK_201/TSK_201_7/Main.java
@@ -0,0 +1,12 @@
+package TSK_201.TSK_201_7;
+
+public class Main {
+    public static void main(String[] arg) {
+        // Ответ в answer.md
+
+        int a = 120;
+        byte b = a + 10;
+        byte c = (byte)(a + 10);
+        byte d = a + 1;
+    }
+}
