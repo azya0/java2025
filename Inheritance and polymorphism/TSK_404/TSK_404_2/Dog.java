044db9f88a273835482fab671dc9901edecb2d6e
Azya <AzyaDr@yandex.ru>
2024-12-09 01:46:22 +0300

    Add TSK_404_2

---
+++ Inheritance and polymorphism/TSK_404/TSK_404_2/Dog.java
@@ -0,0 +1,8 @@
+package TSK_404.TSK_404_2;
+
+class Dog extends AbstractAnimal {
+    @Override
+    public void makeSound() {
+        System.out.println("ГАВ!!!!");
+    }
+}
---
+++ Inheritance and polymorphism/TSK_404/TSK_404_2/Main.java
@@ -0,0 +1,8 @@
+package TSK_404.TSK_404_2;
+
+public class Main {
+    public static void main(String[] args) {
+        AbstractAnimal myDog = new Dog();
+        myDog.makeSound();
+    }
+}
