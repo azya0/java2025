f8092da7d907bae6c3ab3961d11edffe6975e047
Azya <AzyaDr@yandex.ru>
2024-12-09 01:52:23 +0300

    Add TSK_405

---
+++ Inheritance and polymorphism/TSK_405/Main.java
@@ -0,0 +1,22 @@
+package TSK_405;
+
+public class Main {
+    class A {
+        public static void method() {
+            System.out.println("A's static method");
+        }
+    }
+
+    class B extends A {
+        public static void method() {
+            System.out.println("B's static method");
+        }
+    }
+
+    @SuppressWarnings("static-access")
+    public static void main(String[] args) {
+        Main main = new Main();
+        A obj = main.new B();
+        obj.method(); // Выведет: "A's static method"
+    }
+}
