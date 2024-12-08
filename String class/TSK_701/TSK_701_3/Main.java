8d85ca769085f8858a0f3671dbc7f4e10ce1ca56
Azya <AzyaDr@yandex.ru>
2024-12-09 02:31:08 +0300

    Add TSK_701_3

---
+++ String class/TSK_701/TSK_701_3/Main.java
@@ -0,0 +1,13 @@
+package TSK_701.TSK_701_3;
+
+public class Main {
+    public static void main(String[] arg) {
+        String multiLineString = """
+       Ну это типо крутая
+       Модная, молодежная хайповая
+       Многострочная строка
+       """;
+       
+       System.out.println(multiLineString);
+    }
+}
