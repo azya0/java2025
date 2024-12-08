3553ed1e07f0e3105f70144600163d260718c9e8
Azya <AzyaDr@yandex.ru>
2024-12-08 17:58:48 +0300

    Add TSK_205

---
+++ Data types and operators/TSK_205/Main.java
@@ -0,0 +1,10 @@
+package TSK_205;
+
+public class Main {
+    public static void main(String[] arg) {
+        int a = 2;
+        int i = (a == 1) ? 1 : ((a == 2) ? 2 : 3);
+        
+        System.out.println(i);
+    }
+}
