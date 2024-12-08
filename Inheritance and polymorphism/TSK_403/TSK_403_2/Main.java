9aad65b85e1ef2eaac41af84e8a437a543cc2a80
Azya <AzyaDr@yandex.ru>
2024-12-09 01:31:43 +0300

    Update TSK_403_2
    Lost space at the end of Main.java file

--- Inheritance and polymorphism/TSK_403/TSK_403_2/Main.java
+++ Inheritance and polymorphism/TSK_403/TSK_403_2/Main.java
@@ -22,7 +22,7 @@ public class Main {
             super.method();
         }
     }
-    
+
     public static void main(String[] args) {
         Main main = new Main();
         C obj = main.new C();
