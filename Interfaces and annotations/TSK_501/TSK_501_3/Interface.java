df4149bbb79e8e529140c26eac4ad60d460a2f90
Azya <AzyaDr@yandex.ru>
2024-12-09 02:05:53 +0300

    Add TSK_501

---
+++ Interfaces and annotations/TSK_501/TSK_501_3/Interface.java
@@ -0,0 +1,9 @@
+package TSK_501.TSK_501_3;
+
+
+// Тут должен был быть protected, если бы не:
+// Illegal modifier for the interface Interface; only public & abstract are permitted
+
+interface Interface {
+    void myMethod();
+}
---
+++ Interfaces and annotations/TSK_501/TSK_501_4/Interface.java
@@ -0,0 +1,7 @@
+package TSK_501.TSK_501_4;
+
+// Тут должна была быть приватность private, если бы не:
+// Illegal modifier for the interface Interface; only public & abstract are permitted
+interface Interface {
+
+}
