c7f93893d93eac3edd8ecfd8d9dd151747a3070b
Azya <AzyaDr@yandex.ru>
2024-12-08 16:15:55 +0300

    Add TSK_201_3

---
+++ Data types and operators/TSK_201/TSK_201_3/Main.java
@@ -0,0 +1,17 @@
+package TSK_201.TSK_201_3;
+
+public class Main {
+    public static void main(String[] arg) {
+        // Подробнее в answer.md
+
+        int b;
+
+        int a = 1;
+        {
+            b = 2;
+        }
+        int c = a + b;
+
+        System.out.println(c);
+    }
+}
