cbc3952111a50e8e4536f7bd451295e26f6f54a9
Azya <AzyaDr@yandex.ru>
2024-12-09 03:39:51 +0300

    Add TSK_808

---
+++ Exceptions and errors/TSK_808/App.java
@@ -0,0 +1,13 @@
+package TSK_808;
+
+public class App {
+    // Ответ в answer.md
+    final static int START_COUNTER;
+    static {
+        START_COUNTER = Integer.parseInt("Y-");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
