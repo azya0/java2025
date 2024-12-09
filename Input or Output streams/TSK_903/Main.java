4ab05e9202debfc85a66b7f2b87610f67c0d4f71
Azya <AzyaDr@yandex.ru>
2024-12-09 03:51:27 +0300

    Add TSK_903

---
+++ Input or Output streams/TSK_903/Main.java
@@ -0,0 +1,20 @@
+package TSK_903;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("example.txt");
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Файл не найден: " + e.getMessage());
+        }
+    }
+}
